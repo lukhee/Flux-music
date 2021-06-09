346a679a4ec41f1123ad0a8390344f1016ace2ff
BALOGUN LUKMAN <balogunluqman@BALOGUNs-MacBook-Pro.local>
2021-06-09 01:28:09 +0100

    chore: pages and route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  Login,
+  Logout,
+  Register,
+  ForgetPassword,
+} from './components/Pages/Auths/index';
+import HomePage from './components/Pages/Home';
 
 function App() {
   return (
     <div className="text-center text-white min-vh-100 container-fluid bg-secondary">
       Flux Music
+      <Router>
+        <Switch>
+          <Route exact path="/Login">
+            <Login />
+          </Route>
+          <Route exact path="/Logout">
+            <Logout />
+          </Route>
+          <Route exact path="/Register">
+            <Register />
+          </Route>
+          <Route exact path="/ForgetPassword">
+            <ForgetPassword />
+          </Route>
+          <Route exact path="/HomePage">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Pages/Auths/index.js
@@ -0,0 +1,6 @@
+import Login from './Login';
+import Register from './Register';
+import Logout from './Logout';
+import ForgetPassword from './ForgetPassword';
+
+export { Login, Register, Logout, ForgetPassword };
